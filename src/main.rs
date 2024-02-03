2a8ef6213d0cf1a903fbee1e6c322ddd48573457
Ashish Sharda <ashishjsharda@gmail.com>
2024-02-03 05:12:42 -0500

    update main ,add dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+extern crate reqwest;
+extern crate scraper;
+
+use scraper::{Html, Selector};
+use reqwest::blocking::get;
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let url = "https://www.rust-lang.org";
+    let response = reqwest::blocking::get(url)?;
+    assert!(response.status().is_success());
+    let body = response.text()?;
+    let fragment = Html::parse_document(&body);
+    let selector = Selector::parse("a").unwrap();
+    for element in fragment.select(&selector) {
+        let href = element.value().attr("href").unwrap_or_default();
+        println!("{}", href);
+    }
+    Ok(())
 }
